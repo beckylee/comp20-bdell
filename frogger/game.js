65cdc894e94e9ac989805787057d6a4d404610af
Beckylee <Beckylee.Dell@tufts.edu>
2013-03-12 18:59:40 -0400

    Updated lab 5 to become lab 9
    readme added as well.

--- frogger/game.js
+++ frogger/game.js
@@ -1,52 +1,89 @@
 
 function start_game(){
-	canvas = document.getElementById('game');
 
+	canvas = document.getElementById('game');
+	var points = 0;
+	var frog = [];
+	frog.x = 180;
+	frog.y = 475;
+	
 	if (canvas.getContext) {
 	    ctx = canvas.getContext('2d');
-	    ctx.fillStyle = "rgb(19, 19, 70)";
-	    ctx.fillRect (0, 0, 399, 282);
-	    
-	    ctx.fillStyle = "rgb(00, 00, 00)";
-	    ctx.fillRect(0, 282, 399, 282);
-	
 	
 		var img = new Image();
+		
 		img.src = 'assets/frogger_sprites.png';
-
-	// Put in "frogger" at the top, and green ending 
-		ctx.drawImage(img, 0, 0, 399, 100, 0, 0, 399, 100);
+		background();
+		
+		function background(){
+		    ctx.fillStyle = "rgb(19, 19, 70)";
+		    ctx.fillRect (0, 0, 399, 282);
+	    
+		    ctx.fillStyle = "rgb(00, 00, 00)";
+		    ctx.fillRect(0, 282, 399, 282);
 	
-	// Put in large purple borders	
-		ctx.drawImage(img, 0, 120, 399, 30, 0, 283, 399, 40);
-		ctx.drawImage(img, 0, 120, 399, 30, 0, 475, 399, 40);
-		
-	// Put in big logs
-		ctx.drawImage(img, 0, 165, 190, 30, -10, 200, 170, 30);
-		ctx.drawImage(img, 0, 165, 190, 30, 300, 200, 170, 30);
 	
-	// Put in medium logs
-		ctx.drawImage(img, 0, 195, 210, 30, 20, 105, 180, 30);
-		ctx.drawImage(img, 0, 195, 210, 30, 200, 105, 180, 30);
-		ctx.drawImage(img, 0, 195, 210, 30, 385, 105, 180, 30);
-		
-	// Put in small logs
-		ctx.drawImage(img, 0, 226, 100, 26, -25, 150, 90, 25);
-		ctx.drawImage(img, 0, 226, 100, 26, 140, 150, 90, 25);
-		ctx.drawImage(img, 0, 226, 100, 26, 260, 150, 90, 25);
-		ctx.drawImage(img, 0, 226, 100, 26, 375, 150, 90, 25);
-		
-		ctx.drawImage(img, 0, 226, 100, 26, 25, 245, 90, 25);
-		ctx.drawImage(img, 0, 226, 100, 26, 130, 245, 90, 25);
-		ctx.drawImage(img, 0, 226, 100, 26, 290, 245, 90, 25);
+			var img = new Image();
+			img.src = 'assets/frogger_sprites.png';
 		
 	//Text
-		ctx.fillStyle="rgb(0, 255, 0)";
-		ctx.font="20px Verdana";
-		ctx.fillText("Level 1", 50, 535);
+			ctx.fillStyle="rgb(0, 255, 0)";
+			ctx.font="20px Verdana";
+			ctx.fillText("Level 1", 50, 535);
+			
+			ctx.font="11px Verdana";
+			ctx.fillText("Score: "+points +"	Highscore: 0", 0, 555);
+			
+	// Put in "frogger" at the top, and green ending 
+			ctx.drawImage(img, 0, 0, 399, 100, 0, 0, 399, 100);
+	
+	// Put in large purple borders	
+			ctx.drawImage(img, 0, 120, 399, 30, 0, 283, 399, 40);
+			ctx.drawImage(img, 0, 120, 399, 30, 0, 475, 399, 40);
 		
-		ctx.font="11px Verdana";
-		ctx.fillText("Score: 0		Highscore: 0", 0, 555);
+			}
+			
+			
+	delay = 80;
+	setInterval(move, delay);
+	var log = 400;
+	var log2 = 150;
+	function move(){
+	// Put in big logs
+		background();
+		log = log-10;
+		log2 = log2-10;
+
+		ctx.drawImage(img, 0, 165, 190, 30, log, 200, 170, 30);
+		
+		ctx.drawImage(img, 0, 165, 190, 30, log2, 200, 170, 30);
+
+		if(log < -200){
+			log = 420;
+		}		
+		
+		if(log2 < -200){
+			log2 = 420;
+		}
+		
+		ctx.drawImage(img, 40, 360, 30, 30, frog.x, frog.y, 25, 25);
+
+	
+	// Put in medium logs
+//		ctx.drawImage(img, 0, 195, 210, 30, 20, 105, 180, 30);
+//		ctx.drawImage(img, 0, 195, 210, 30, 200, 105, 180, 30);
+//		ctx.drawImage(img, 0, 195, 210, 30, 385, 105, 180, 30);
+		
+	// Put in small logs
+//		ctx.drawImage(img, 0, 226, 100, 26, -25, 150, 90, 25);
+//		ctx.drawImage(img, 0, 226, 100, 26, 140, 150, 90, 25);
+//		ctx.drawImage(img, 0, 226, 100, 26, 260, 150, 90, 25);
+//		ctx.drawImage(img, 0, 226, 100, 26, 375, 150, 90, 25);
+		
+//		ctx.drawImage(img, 0, 226, 100, 26, 25, 245, 90, 25);
+//		ctx.drawImage(img, 0, 226, 100, 26, 130, 245, 90, 25);
+//		ctx.drawImage(img, 0, 226, 100, 26, 290, 245, 90, 25);
+
 
 	// Life frogs
 	
@@ -57,12 +94,84 @@ function start_game(){
 		ctx.drawImage(img, 0, 252, 45, 45, 20, 380, 45, 45);
 
 	// white red car
-		ctx.drawImage(img, 40, 252, 40, 45, 200, 340, 40, 45);
+		ctx.drawImage(img, 40, 252, 40, 45, 200, 347, 40, 45);
 		
-	// start frog
-		ctx.drawImage(img, 40, 360, 30, 30, 175, 478, 30, 30);
-		
-
 	}
+	
+	// start frog
+		ctx.drawImage(img, 40, 360, 30, 30, 180, 475, 25, 25);
+	}
+
+// control frogger
+	document.addEventListener("keydown", function(event){
+	
+	//go up
+		if(event.keyCode == 38){
+			background();
+			frog.y = frog.y - 30;
+			if(frog.y == 295){
+				frog.y = 285;
+			}
+			if(frog.y < 285){
+				frog.y = frog.y - 15;
+			}
+			if(frog.y == 60){
+				frog.y = 70;
+			}
+			if(frog.y < 60){
+				frog.y = 70;
+				if(frog.x == 30|| frog.x == 0){
+					frog.x == 20;
+				}
+			}
+			ctx.drawImage(img, 40, 360, 30, 30, frog.x, frog.y, 25, 25);
+			
+			points += 10;
+			
+			console.log(frog.y);
+		}
+		
+	// go down
+		if(event.keyCode == 40){
+			background();
+			frog.y = frog.y + 30;
+			if(frog.y == 285){
+				frog.y = 295
+			}
+			if(frog.y < 285){
+				frog.y = frog.y+15;
+			}
+			if(frog.y >= 470){
+				frog.y = 475;
+			}
+			ctx.drawImage(img, 40, 360, 30, 30, frog.x, frog.y, 25, 25);
+
+		}
+		
+	//go left
+		if(event.keyCode == 37){
+			background();
+			frog.x = frog.x - 40;
+			
+			if(frog.x < 20){
+				frog.x = 20;
+			}
+
+			ctx.drawImage(img, 40, 360, 30, 30, frog.x, frog.y, 25, 25);
+		}
+	
+	//go right
+		if(event.keyCode == 39){
+			background();
+			frog.x = frog.x + 40;
+			if(frog.x > 340){
+				frog.x = 340;
+			}
+			ctx.drawImage(img, 40, 360, 30, 30, frog.x, frog.y, 25, 25);
+		}
+	
+		});
+	
+
 }
 
---
+++ status_dom/README.txt
@@ -0,0 +1,8 @@
+Lab 9
+Beckylee Dell
+
+1. for some reason the time updates to current time when you refresh it
+
+2. no help on this one
+
+3. about 30 minutes
