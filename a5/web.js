2cc3aa911e298b42e263cb5b454cb0875343dc33
Beckylee <Beckylee.Dell@tufts.edu>
2013-04-16 13:02:30 -0400

    added a little bit of everything
    created the framework for each page/requirement of the assignment.

--- a5/web.js
+++ a5/web.js
@@ -5,41 +5,70 @@ app.use(express.logger());
 
 var MONGOHQ_URL = 'mongodb://beckylee.dell@tufts.edu:Flopsy@dharma.mongohq.com:10042/scores';
     
-
+//get info for top score from user
 app.get('/submit.json', function(request, response){
 	response.sendfile(__dirname + '/submit.html');
-	});
+	// do I need to send info from here to the post below?
+});
 	
-
-
-/*
+//Save the info
 app.post('/submit.json', function(request, response){
-	
-	response.send("hi");	
+	//send information to mongodb somehow.
+	response.send("Information saved! (eventually)" + game_title);	
 
 });
-*/
 
+
+//Allow user input for game, then search mongodb for that game
 app.get('/highscores.json', function(request, response){
 	
+		response.sendfile(__dirname + '/highscores.html');
+		//get the title and pass it to mongodb
 		var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/scorecenter';
 		var mongo = require('mongodb');
 
 		var db = mongo.Db.connect(mongoUri, {safe:true}, function(error, scorecenter){
-			if (error) throw error;
 			
 			db = scorecenter;
 			console.log(scorecenter);
-	
+		// go through and find top ten
 		});
 
-	response.send("database is: " + db);
+});
 
+// Post the top 10 scores
+app.post('/highscores.json', function(request, response){
+	response.send('Top scores!');
+	//show top ten scores
 });
 	
 
+//homepage should show top scores for all games.
 app.get('/', function(request, response){
-	response.send('Hello FACE!');
+		var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/scorecenter';
+		var mongo = require('mongodb');
+
+		var db = mongo.Db.connect(mongoUri, {safe:true}, function(error, scorecenter){
+			
+			db = scorecenter;
+			console.log(scorecenter);
+		// find and list all of the top scores
+		});
+	response.send("top scores: " + db);
+});
+
+
+
+//user input for a username
+app.get('/usersearch.json', function(request, response){
+	response.sendfile(__dirname + '/usersearch.html');
+	// search mongodb for that name
+});
+
+//show that user's scores
+app.post('/usersearch.json', function(request, response){
+	response.send('Scores for that user!');
+	//show those scores
 });
 
 var port = process.env.PORT || 5000;
